9cabbe23180da1c6a37393c2e9a081b01997ffbd
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2021-08-09 19:57:28 +0100

    new component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
-import './App.css';
-import { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
-
+import "./App.css";
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 // function App() {
 //   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
 //   );
 // }
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
 
   componentDidMount() {
     let url = "https://jsonplaceholder.typicode.com/users";
     fetch(url)
-    .then(response => response.json())
-    .then(data => this.setState( {monsters: data} ))
+      .then((response) => response.json())
+      .then((data) => this.setState({ monsters: data }));
   }
   render() {
     return (
       <div className="App">
-        <CardList monsters = {this.state.monsters} />
+        <input type="search" placeholder="Search Monsters" />
+        <CardList monsters={this.state.monsters} />
       </div>
-    )
+    );
   }
 }
+
+// setState is an asynchronous function. If you want to do something only after setState has rendered, make a callback that will get called right after setState.
+
+// You cannot run setState in render()
 
 export default App;
